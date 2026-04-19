61c9448b59b8da8f9c796be27ec7bcc33159cbce
shap0011 <shap0011@algonquinlive.com>
2026-04-19 04:33:41 -0400

    Remove duplicate backend bootstrap (using environments/backend)

--- terraform/backend/main.tf
+++
@@ -1,8 +0,0 @@
-module "backend" {
-  source = "../modules/backend"
-
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  storage_account_name = var.storage_account_name
-  container_name       = var.container_name
-}
